2cbb25848f2a134c172127b4c1fb5d9cc05aeba2
Fábio Santos <me@fabiofdsantos.com>
2022-03-10 17:53:46 +0000

    refactor: use strict comparison on `in_array()`

--- src/ORM/Query.php
+++ src/ORM/Query.php
@@ -6,19 +6,24 @@ use Cake\ORM\Query as CakeQuery;
 class Query extends CakeQuery
 {
     /**
-     * Cake\ORM\Query::triggerBeforeFind overwritten to add the condition `deleted IS NULL` to every find request
-     * in order not to return soft deleted records.
+     * Cake\ORM\Query::triggerBeforeFind overwritten to add the condition `deleted IS NULL` to 
+     * every find request in order not to return soft deleted records.
+     *
      * If the query contains the option `withDeleted` the condition `deleted IS NULL` is not applied.
+     *
+     * @return void
      */
     public function triggerBeforeFind(): void
     {
         if (!$this->_beforeFindFired && $this->_type === 'select') {
             parent::triggerBeforeFind();
+            
+            $options = $this->getOptions();
 
-            $aliasedField = $this
-                ->getRepository()
-                ->aliasField($this->getRepository()->getSoftDeleteField());
-            if (!is_array($this->getOptions()) || !in_array('withDeleted', $this->getOptions())) {
+            if (!is_array($options) || !in_array('withDeleted', $options, true)) {
+                $repository = $this->getRepository();
+                $aliasedField = $repository->aliasField($repository->getSoftDeleteField());
+
                 $this->andWhere($aliasedField . ' IS NULL');
             }
         }
